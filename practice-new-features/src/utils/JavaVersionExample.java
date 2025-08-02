64d4cecd70166f6d8d856ed3a779bf4007ed99f8
Kumar <Kumar@10.0.0.164>
2025-08-02 14:35:37 -0400

    string format updates

--- practice-new-features/src/utils/JavaVersionExample.java
+++ practice-new-features/src/utils/JavaVersionExample.java
@@ -3,10 +3,15 @@ package utils;
 public class JavaVersionExample {
 
 	public static void main(String[] args) {
-
-		System.out.println("inside main method");
+		
 		String version = System.getProperty("java.version");
-		System.out.println("version=" + version);
+		System.out.println("java.version=" + version);		
+		
+		System.out.println(String.format("%s %n", "All System Properties"));
+		
+		System.getProperties().forEach((obj1, obj2) -> {
+			System.out.println(obj1 + " :: " + obj2);
+		});
 
 	}
 
